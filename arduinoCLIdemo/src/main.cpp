a6f3875e3183d1a701051538dd676f1ce94d5588
salochinYom <nrmoy2@gmail.com>
2022-06-24 18:01:35 -0400

    sanity LED check

--- arduinoCLIdemo/src/main.cpp
+++ arduinoCLIdemo/src/main.cpp
@@ -2,8 +2,18 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //blink intergrated led on pin 13
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
 }
