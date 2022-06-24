bcedcc41b341123d3b5a128491757ec92bb4b28f
salochinYom <nrmoy2@gmail.com>
2022-06-24 18:23:43 -0400

    moved led to nonblocking soft timer

--- arduinoCLIdemo/src/main.cpp
+++ arduinoCLIdemo/src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <Timer.h>
+
+//led
+Timer ledTimer(1000);
+bool LEDstate = HIGH;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,11 +16,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  //blink intergrated led on pin 13 and print hello every 2sec
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13,LOW);
-  delay(1000);
+  //blink intergrated led on pin 13 print hello every 2sec
+  if(ledTimer.isExpired()){
+    digitalWrite(13, LEDstate);
+    //invert state
+    LEDstate = !LEDstate;
+  }
+
 
   Serial.print("hello \n");
 }
