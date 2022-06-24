b3b0aae5e63ff7a2826f9f51789c8f40ed199298
salochinYom <nrmoy2@gmail.com>
2022-06-24 18:15:30 -0400

    added print to serial

--- arduinoCLIdemo/src/main.cpp
+++ arduinoCLIdemo/src/main.cpp
@@ -2,18 +2,20 @@
 
 void setup() {
   // put your setup code here, to run once:
+  //setup the pin
   pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13,LOW);
-  delay(1000);
+  //setup the serial output that goes to the computer
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //blink intergrated led on pin 13
+
+  //blink intergrated led on pin 13 and print hello every 2sec
   digitalWrite(13, HIGH);
   delay(1000);
   digitalWrite(13,LOW);
   delay(1000);
+
+  Serial.print("hello \n");
 }
